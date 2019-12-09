7ccd90ddf0d713319703dc51557c1f9ec5f5c55a
Naoki Okada <46291090+okadan@users.noreply.github.com>
2019-12-09 11:34:02 +0900

    Removed dependency on meta package

--- lib/nfc_manager.dart
+++ lib/nfc_manager.dart
@@ -4,8 +4,8 @@ import 'dart:convert' show utf8, ascii;
 import 'dart:typed_data' show Uint8List;
 import 'dart:ui' show Locale;
 
+import 'package:flutter/foundation.dart' show required;
 import 'package:flutter/services.dart' show MethodChannel, MethodCall;
-import 'package:meta/meta.dart';
 
 part 'src/ndef.dart';
 part 'src/nfc.dart';
